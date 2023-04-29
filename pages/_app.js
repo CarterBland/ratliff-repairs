9c6412b107258f0bf35f1e3ccb7a1230de46212f
Carter Bland <cb@buildmidwestern.com>
2023-04-29 11:19:29 -0500

    fix: new stuff

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,28 @@
-import '@styles/globals.css'
+import Head from 'next/head';
+import PropTypes from 'prop-types';
+import Layout from '../components/layout/layout';
+import { ScrollToTop } from '../components/scroll';
+import '../styles/globals.css';
 
-function Application({ Component, pageProps }) {
-  return <Component {...pageProps} />
+function MyApp({ Component, pageProps }) {
+    return (
+        <Layout>
+            <Head>
+                <meta
+                    name="viewport"
+                    content="width=device-width, initial-scale=1"
+                />
+                <link rel="shortcut icon" href="../favicon.png" />
+            </Head>
+            <Component {...pageProps} />
+            <ScrollToTop />
+        </Layout>
+    );
 }
 
-export default Application
+MyApp.propTypes = {
+    Component: PropTypes.instanceOf(Object).isRequired,
+    pageProps: PropTypes.instanceOf(Object).isRequired,
+};
+
+export default MyApp;
